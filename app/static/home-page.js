3575f4177fdae9a9c7a60774c61ad1dd7e2f4346
RJay-Pickering <rjaypickering@gmail.com>
2022-12-22 15:14:41 -0600

    fixed a minor problem with the deletion button

--- app/static/home-page.js
+++ app/static/home-page.js
@@ -1,39 +1,45 @@
-var btn = document.querySelector( '.btn-front-1' );
+var btn = document.querySelectorAll( '.btn-front-1' );
 
-var btnFront = btn.querySelector( '.btn-front' ),
-    btnYes = btn.querySelector( '.btn-back .yes' ),
-    btnNo = btn.querySelector( '.btn-back .no' );
+var btnFront
+var btnYes
+var btnNo
 
-btnFront.addEventListener( 'click', function( event ) {
-  var mx = event.clientX - btn.offsetLeft,
-      my = event.clientY - btn.offsetTop;
+for (let i = 0; i < btn.length; i++) {
+  btnFront = btn[i].querySelector( '.btn-front' ),
+  btnYes = btn[i].querySelector( '.btn-back .yes' ),
+  btnNo = btn[i].querySelector( '.btn-back .no' );
 
-  var w = btn.offsetWidth,
-      h = btn.offsetHeight;
-	
-  var directions = [
-    { id: 'top', x: w/2, y: 0 },
-    { id: 'right', x: w, y: h/2 },
-    { id: 'bottom', x: w/2, y: h },
-    { id: 'left', x: 0, y: h/2 }
-  ];
-  
-  directions.sort( function( a, b ) {
-    return distance( mx, my, a.x, a.y ) - distance( mx, my, b.x, b.y );
+  btnFront.addEventListener( 'click', function( event ) {
+    var mx = event.clientX - btn[i].offsetLeft,
+        my = event.clientY - btn[i].offsetTop;
+
+    var w = btn[i].offsetWidth,
+        h = btn[i].offsetHeight;
+    
+    var directions = [
+      { id: 'top', x: w/2, y: 0 },
+      { id: 'right', x: w, y: h/2 },
+      { id: 'bottom', x: w/2, y: h },
+      { id: 'left', x: 0, y: h/2 }
+    ];
+    
+    directions.sort( function( a, b ) {
+      return distance( mx, my, a.x, a.y ) - distance( mx, my, b.x, b.y );
+    } );
+    
+    btn[i].setAttribute( 'data-direction', directions.shift().id );
+    btn[i].classList.add( 'is-open' );
+
+  } );
+
+  btnYes.addEventListener( 'click', function( event ) {	
+    btn[i].classList.remove( 'is-open' );
   } );
   
-  btn.setAttribute( 'data-direction', directions.shift().id );
-  btn.classList.add( 'is-open' );
-
-} );
-
-btnYes.addEventListener( 'click', function( event ) {	
-  btn.classList.remove( 'is-open' );
-} );
-
-btnNo.addEventListener( 'click', function( event ) {
-  btn.classList.remove( 'is-open' );
-} );
+  btnNo.addEventListener( 'click', function( event ) {
+    btn[i].classList.remove( 'is-open' );
+  } );
+}
 
 function distance( x1, y1, x2, y2 ) {
   var dx = x1-x2;
