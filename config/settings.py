4c07a7ff9e010f08f4823abef63dde4913465638
RJay-Pickering <rjaypickering@gmail.com>
2023-01-06 13:25:33 -0600

    E

--- config/settings.py
+++ config/settings.py
@@ -137,3 +137,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['missing-cats-of-america-production.up.railway.app']
